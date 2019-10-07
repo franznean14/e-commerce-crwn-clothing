64742c3b3122df5bb96a03a168f59e8be31e1991
Franz Casabuena <franzneandra@gmail.com>
2019-10-07 13:22:07 +0800

    added shop page, collection preview comp and collection item comp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
-import HomePage from './pages/homepage.component';
+
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+        </Switch>
+      </Router>
   );
 }
 
