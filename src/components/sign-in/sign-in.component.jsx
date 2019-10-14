09965965f80a6852fa0af35b08d8caea2cbce3be
Franz Casabuena <franzneandra@gmail.com>
2019-10-14 17:21:21 +0800

    finished sign in handle button function

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 export class SignIn extends Component {
     constructor(props) {
@@ -20,12 +20,20 @@ export class SignIn extends Component {
         this.handleChange = this.handleChange.bind(this);
     }
     
-    handleSubmit(e){
-        e.preventDefault();
-        this.setState({
-            email: '',
-            password: ''
-        })
+    handleSubmit = async event => {
+        event.preventDefault();
+        
+        const {email, password} = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({
+                email: '',
+                password: ''
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     handleChange(event){
